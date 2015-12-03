774ac29f8c18d58c86e6834601771bd5f8048c93
test111 <test@gmail.com>
2015-12-02 20:49:14 -0800

    full name letter counter

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,18 @@
-my_stats = {"name": "hilary", "age": "31", "height": "5'3"} 
-print my_stats["name"] + " is " + my_stats["age"] + " years old with a height of " + my_stats["height"]
+#my_stats = {"name": "hilary", "age": "31", "height": "5'3"} 
+#print my_stats["name"] + " is " + my_stats["age"] + " years old with a height of " + my_stats["height"]
+#del my_stats["age"]
+#print my_stats 
+
+#vocabulary_words = {"integer": "a whole number", "float": "a decimal number", 
+#"boolen": "a conditional stating whether it is true or false", "string": "a value within quotation marks"}
+#print vocabulary_words
+
+full_name = raw_input("What is your full name? ")
+namedictionary = {}
+for letter in full_name:
+	if letter in namedictionary:
+		namedictionary[letter] += 1 
+	else:
+		namedictionary[letter] = 1 
+print namedictionary
+
