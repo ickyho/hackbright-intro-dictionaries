90e25d89c2859332bd79b46fb447759e4a3c1dc4
test111 <test@gmail.com>
2015-12-02 21:16:26 -0800

    fish

--- dictionaries.py
+++ dictionaries.py
@@ -7,12 +7,22 @@
 #"boolen": "a conditional stating whether it is true or false", "string": "a value within quotation marks"}
 #print vocabulary_words
 
-full_name = raw_input("What is your full name? ")
-namedictionary = {}
-for letter in full_name:
-	if letter in namedictionary:
-		namedictionary[letter] += 1 
-	else:
-		namedictionary[letter] = 1 
-print namedictionary
+#full_name = raw_input("What is your full name? ")
+#namedictionary = {}
+#for letter in full_name:
+#	if letter in namedictionary:
+#		namedictionary[letter] += 1 
+#	else:
+#		namedictionary[letter] = 1
+#print namedictionary
 
+
+fish = raw_input("Give me a string!")
+
+fishdictionary = {}
+for word in fish:
+	if word in fishdictionary:
+		fishdictionary[word] += 1
+	else:
+		fishdictionary[word] = 1
+print fishdictionary
